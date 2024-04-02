6ecfbe0a630aebe1580e45402a048df1c65927e1
Chloe <cgreenstreet01@gmail.com>
2024-04-02 18:55:25 +0100

    Recipe : Validation tests

--- src/test/java/org/example/menu/drinks/drink/RecipeTest.java
+++ src/test/java/org/example/menu/drinks/drink/RecipeTest.java
@@ -3,6 +3,7 @@ package org.example.menu.drinks.drink;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 class RecipeTest {
@@ -11,5 +12,26 @@ class RecipeTest {
         assertThatNullPointerException()
                 .isThrownBy(() -> Recipe.of(null, Base.MILK, ContainerType.GLASS))
                 .withMessage("shot must not be null");
+    }
+
+    @Test
+    void baseMustNotBeNull() {
+        assertThatNullPointerException()
+                .isThrownBy(() -> Recipe.of(1, null, ContainerType.GLASS))
+                .withMessage("base must not be null");
+    }
+
+    @Test
+    void containerTypeMustNotBeNull() {
+        assertThatNullPointerException()
+                .isThrownBy(() -> Recipe.of(0, Base.WATER, null))
+                .withMessage("containerType must not be null");
+    }
+
+    @Test
+    void shotNumberMustBeBiggerThanZero() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Recipe.of(-1, Base.WATER, ContainerType.GLASS))
+                .withMessage("Invalid Shot Amount");
     }
 }
